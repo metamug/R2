5c556f75600abb0cd53140a44ff6b747e6b63bc3
Anish Hirlekar <anish.hirlekar@metamug.com>
2016-01-07 13:36:27 +0530

    Added validation code. First the xml is validated with the xsd. Any errors in the xml or the xsd itself throw exception. Only after validation, the xml is parsed into java objects.

--- src/main/java/com/metamug/jaxb/JAXBParser.java
+++ src/main/java/com/metamug/jaxb/JAXBParser.java
@@ -9,29 +9,60 @@ package com.metamug.jaxb;
 import com.metamug.jaxb.gener.Request;
 import com.metamug.jaxb.gener.Resource;
 import com.metamug.jaxb.gener.Sql;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author anish
  */
-public class JAXB {
-    public static void main(String[] args){
-        try {
+public class JAXBParser {
+    public static void main(String[] args) throws SAXException{
+        File xml = new File(JAXBParser.class.getResource("/apple.xml").getFile());
+        File xsd = new File(JAXBParser.class.getResource("/apple.xsd").getFile());
+        Source xmlFile = new StreamSource(xml);
+	SchemaFactory schemaFactory = SchemaFactory
+                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+	Schema schema = schemaFactory.newSchema(xsd);
+	Validator validator = schema.newValidator();
+	try {
+            validator.validate(xmlFile);
+            parse();
+        } catch (SAXException|IOException e) {
+	    System.out.println(xmlFile.getSystemId() + " is NOT valid.");
+	    System.out.println("Reason: " + e.getMessage());
+	}		
+       
+    }
+    
+    public static void parse(){
+        try {            
             JAXBContext jaxbContext = JAXBContext.newInstance(Resource.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            Resource resource = (Resource) jaxbUnmarshaller.unmarshal(JAXB.class.getResourceAsStream("/apple.xml"));
+            Resource resource = (Resource) jaxbUnmarshaller.unmarshal(
+                    JAXBParser.class.getResourceAsStream("/apple.xml"));
             
             System.out.println("table: " + resource.getTable());
-            System.out.print("    version: " + Float.toString(resource.getVersion()));
-            //
+            System.out.println("    version: " + Float.toString(resource.getVersion()));
+            System.out.println("    desc: " + resource.getDesc());
+            System.out.print("\n--------------------------------------------------------------");
+            
             for (Request req : resource.getRequestOrCreateOrRead()) {
                 System.out.println("\n--------------------------------------------------------------");
                 System.out.print("    method: " + req.getMethod().value());
                 System.out.print("    out: " + req.getOut().value());
-                System.out.print("    id: " + req.getId());               
+                System.out.println("    id: " + req.getId());               
+                System.out.println("    desc: " + req.getDesc());
                 
                 for(Sql sql : req.getSql()){
                     System.out.println("    sqltype: " + sql.getType());
@@ -39,7 +70,6 @@ public class JAXB {
                     System.out.println(sql.getValue().trim()); 
                 }
             }
-            //it is because  of the spcae in the print statements
         } catch (JAXBException e) {
             e.printStackTrace();
 	}
