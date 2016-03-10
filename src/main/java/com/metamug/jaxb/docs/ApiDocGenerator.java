d1d245534213ae0bf4ac8179ec66846ba216355f
Anish Hirlekar <anish.hirlekar@metamug.com>
2016-03-10 19:05:10 +0530

    Removed startRow and maxRow attributes from Sql element. Removed paramVar and paramVal children from execute element. Changed APIDocGenerator and resource xsl to add CSS.

--- src/main/java/com/metamug/jaxb/docs/ApiDocGenerator.java
+++ src/main/java/com/metamug/jaxb/docs/ApiDocGenerator.java
@@ -5,14 +5,22 @@
  */
 package com.metamug.jaxb.docs;
 
-import java.io.BufferedWriter;
+import com.sun.xml.txw2.output.IndentingXMLStreamWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.xpath.XPathExpressionException;
 
 /**
  *
@@ -20,51 +28,87 @@ import java.util.logging.Logger;
  */
 public class ApiDocGenerator {
     
-    public static void generate(String directoryAddress){
-        
-        List<String> htmlFilenameList = new ArrayList<>();
-        
-        File directory = new File(directoryAddress);
-        File[] listOfFiles = directory.listFiles();
-                
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                if(listOfFiles[i].getName().endsWith(".html")){
-                    htmlFilenameList.add(listOfFiles[i].getName());
-                }
-            }               
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append("<!DOCTYPE html><html>");
-        sb.append("<head><title>Welcome</title></head>");
-        sb.append("<body>");
-            
-        
-        if(!htmlFilenameList.isEmpty()){
-            sb.append("<ul>");
-            for(String name : htmlFilenameList){    
-                sb.append("<li><a href=\"#\">");
-                sb.append(name);
-                sb.append("</a></li>");
-            }
-            sb.append("</ul>");
-        } else{
-            sb.append("<h4>No resources created!</h4>");
-        }
-            
-        sb.append("</body></html>");
-               
+    public static void generate(String appDirectory) {
+
         try {
-            File outFile = new File(directoryAddress + "\\doc\\welcome_page" + ".html");
-            outFile.createNewFile();
-            BufferedWriter bw = new BufferedWriter(new FileWriter(outFile));
-            bw.write(sb.toString());
-            bw.close();
+            List<String> htmlFilenameList = new ArrayList<>();
+
+            File directory = new File(appDirectory + "/docs");
+            File[] listOfFiles = directory.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".html");
+                }
+            });
+            directory.listFiles((File dir, String name) -> name.toLowerCase().endsWith(".html"));
+            for (File file : listOfFiles) {
+                htmlFilenameList.add(file.getName());
+            }
             
-        } catch (IOException ex) {
+            XMLOutputFactory factory = XMLOutputFactory.newInstance();
+            OutputStream output = new FileOutputStream(appDirectory + "/index" + ".html");
+            XMLStreamWriter writer = new IndentingXMLStreamWriter(factory.createXMLStreamWriter(output, "UTF-8"));
+            writer.writeStartElement("html");
+            writer.writeStartElement("head");
+            writer.writeStartElement("meta");
+            writer.writeAttribute("charset", "utf-8");
+            writer.writeEndElement();
+            writer.writeStartElement("meta");
+            writer.writeAttribute("http-equiv", "X-UA-Compatible");
+            writer.writeAttribute("content", "IE=edge");
+            writer.writeEndElement();
+            writer.writeStartElement("meta");
+            writer.writeAttribute("name", "viewport");
+            writer.writeAttribute("content", "width=device-width, initial-scale=1, shrink-to-fit=no");
+            writer.writeEndElement();
+            writer.writeStartElement("link");
+            writer.writeAttribute("href", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css");
+            writer.writeAttribute("rel", "stylesheet");
+            writer.writeEndElement();
+            writer.writeStartElement("title");
+            writeEscapedCharacters("Welcome",writer,output);
+            writer.writeEndElement();   //End of </title>
+            writer.writeEndElement();   //End of </head>
+            writer.writeStartElement("body");
+            writer.writeStartElement("div");
+            writer.writeAttribute("class", "container");
+            writer.writeAttribute("style", "padding-top:25px");
+            writer.writeStartElement("h2");
+            writeEscapedCharacters("Resource List:",writer,output);
+            writer.writeEndElement();
+            if (!htmlFilenameList.isEmpty()) {
+                writer.writeStartElement("div");
+                writer.writeAttribute("class", "list-group col-md-4");
+                for (String name : htmlFilenameList) {
+                    writer.writeStartElement("a");
+                    writer.writeAttribute("class", "list-group-item");
+                    writer.writeAttribute("href", "docs\\" + name);
+                    writeEscapedCharacters(name,writer,output);
+                    writer.writeEndElement();   //Endo of </a>
+                }
+
+                writer.writeEndElement();   //Endo of </div class="list-group">
+            } else {
+                writer.writeStartElement("h4");
+                writeEscapedCharacters("No resources created!",writer,output);
+                writer.writeEndElement();   //Endo of </h4>
+            }
+            writer.writeEndElement();   //End of </div class="container">
+            writer.writeEndElement();   //End of </body>
+            writer.writeEndElement();   //End of </html>
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ApiDocGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (XMLStreamException | XPathExpressionException | IOException ex) {
             Logger.getLogger(ApiDocGenerator.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    private static void writeEscapedCharacters(String data, XMLStreamWriter writer, OutputStream output)
+            throws XMLStreamException, IOException, XPathExpressionException {
+        writer.writeCharacters("");
+        writer.flush();
+        OutputStreamWriter osw = new OutputStreamWriter(output);
+        osw.write(data);
+        osw.flush();
+    }
 }
