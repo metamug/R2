20d7d8fb0ab8d35e81fdad4e75fddfe733ab1ff0
Deepak Mishra <deepak.mishra@metamug.com>
2016-12-21 20:43:47 +0530

    Separated Test Resources. Updated Test

--- src/test/java/com/metamug/jaxb/tests/ParamValidationTest.java
+++ src/test/java/com/metamug/jaxb/tests/ParamValidationTest.java
@@ -16,63 +16,49 @@ import org.junit.Test;
  * @author anish
  */
 public class ParamValidationTest {
+
     Param param1, param2, param3, regexParam1;
-    
+
     @Before
-    public void init(){
+    public void init() {
         param1 = new Param();
         param1.setBlank(Boolean.FALSE);
         param1.setNum(Boolean.FALSE);
         param1.setMaxLen("10");
         param1.setMinLen("4");
-        
+
         param2 = new Param();
         param2.setBlank(Boolean.FALSE);
         param2.setNum(Boolean.TRUE);
         param2.setMax("100");
         param2.setMin("10");
-        
+
         param3 = new Param();
         param3.setBlank(Boolean.TRUE);
-        
+
         regexParam1 = new Param();
         regexParam1.setPattern("[a-zA-Z]{3}");
     }
-    
-    @Test(expected=InputValidationException.class)
-    public void InvalidTest1() throws InputValidationException{
+
+    @Test(expected = InputValidationException.class)
+    public void InvalidTest1() throws InputValidationException {
         JAXBParser.validateParam(param1, "par");
     }
-    
-    @Test(expected=InputValidationException.class)
-    public void InvalidTest2() throws InputValidationException{
+
+    @Test(expected = InputValidationException.class)
+    public void InvalidTest2() throws InputValidationException {
         JAXBParser.validateParam(param2, "1000");
-    }
-    
-    @Test(expected=InputValidationException.class)
-    public void InvalidTest3() throws InputValidationException{
         JAXBParser.validateParam(param2, "");
-    }
-    
-    @Test(expected=InputValidationException.class)
-    public void InvalidTest4() throws InputValidationException{
         JAXBParser.validateParam(param2, "No");
-    }
-    
-    @Test(expected=InputValidationException.class)
-    public void InvalidTest5() throws InputValidationException{
         JAXBParser.validateParam(param2, "123");
     }
-    
-    @Test
-    public void ValidTest1() throws InputValidationException{
+
+//    @Test
+    public void ValidTest1() throws InputValidationException {
         JAXBParser.validateParam(param1, "param123");
         JAXBParser.validateParam(param1, "paramStr");
-        
         JAXBParser.validateParam(param2, "12");
-        
         JAXBParser.validateParam(param3, "");
-        
         JAXBParser.validateParam(regexParam1, "abc");
     }
 }
