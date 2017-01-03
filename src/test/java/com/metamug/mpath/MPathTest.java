ba42fca971119de7a66227b7e2a449370a13bf98
anishhirlekar <anishhirlekar@anishs-air>
2017-01-03 15:52:09 +0530

    added test cases to compare values returned from xml input and equivalent json input for the same mpath input

--- src/test/java/com/metamug/mpath/MPathTest.java
+++ src/test/java/com/metamug/mpath/MPathTest.java
@@ -10,7 +10,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import org.json.JSONObject;
 import org.json.XML;
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -19,7 +20,6 @@ import org.xml.sax.SAXException;
  * @author anishhirlekar
  */
 public class MPathTest {
-    public static String testXml;
     public static final String TEST_JSON = "{\n" +
       "   \"Port\":\n" +
       "   {\n" +
@@ -40,7 +40,7 @@ public class MPathTest {
       "           [                         \n" +
       "               {\n" +
       "                   \"name\": \"connectionTimeout\",\n" +
-      "                   \"D\": \"20000\"\n" +
+      "                   \"D\": 20000\n" +
       "               },\n" +
       "               {\n" +
       "                   \"name\": \"connectionTimeout_Again!\",\n" +
@@ -51,31 +51,77 @@ public class MPathTest {
       "   }\n" +
       "}";
     
-    @Before
-    public void init(){
-        JSONObject jobj = new JSONObject(TEST_JSON);
-        //System.out.println(jobj);
-        testXml = XML.toString(jobj);
-    }
+    public static final String TEST_XML = "<Resource version=\"1.1\" >\n" +
+            "\n" +
+            "	<Desc>Contains records for data type apple</Desc>\n" +
+            "\n" +
+            "	<Request method=\"GET\" item=\"true\" status=\"200\">\n" +
+            "		<Param name=\"t\" blank=\"true\" min=\"1\" exists=\"table.column\" />\n" +
+            "                <Param name=\"u\" min=\"1\" max=\"100\" pattern=\"pat341./1@[]^\" />\n" +
+            "                \n" +
+            "                <Sql when=\"@q eq 1\" type=\"query\">\n" +
+            "                    select * from employee where employee_name = @v\n" +
+            "                </Sql>\n" +
+            "                <Sql when=\"@q eq 3\" type=\"update\">\n" +
+            "                    update employee set employee_name = @param2 where employee_id=@id\n" +
+            "                </Sql>\n" +
+            "	</Request>\n" +
+            "	       \n" +
+            "	<Request method=\"POST\" status=\"201\">\n" +
+            "		<Desc>Request for creating an employee record.</Desc>\n" +
+            "		<Sql type=\"update\">\n" +
+            "		insert into employee \n" +
+            "		(employee_name, employee_id) \n" +
+            "		values(@name, @id)\n" +
+            "		</Sql>\n" +
+            "\n" +
+            "	</Request>\n" +
+            "\n" +
+            "	<!--derived from request -->\n" +
+            "	<Request method=\"PUT\" item=\"true\">\n" +
+            "		<Execute className=\"com.mtg.Apple\" />\n" +
+            "	</Request>\n" +
+            "	\n" +
+            "</Resource>\n" +
+            "";
     
+    @Ignore
     @Test
-    public void MPathTest() throws XPathExpressionException, IOException,
+    public void TestCase1() throws XPathExpressionException, IOException,
                                     SAXException, ParserConfigurationException{
-        //String xPath3 = "/Port/ThreadPool/Max";
         
+        String testXml = XML.toString(new JSONObject(TEST_JSON));
         String mKey1 = "Port.ExtendedProperties.Property[0].D";
         String mKey2 = "Port.ExtendedProperties.Property[1].D";
         String mKey3 = "Port.ThreadPool.Max";
-        String jsonVal1 = MPathUtil.getValueFromJson(TEST_JSON, mKey1);
-        String jsonVal2 = MPathUtil.getValueFromJson(TEST_JSON, mKey2);
-        String jsonVal3 = MPathUtil.getValueFromJson(TEST_JSON, mKey3);
-        System.out.println(jsonVal1);
-        System.out.println(jsonVal2);
+        Object jsonVal1 = MPathUtil.getValueFromJson(TEST_JSON, mKey1);
+        Object jsonVal2 = MPathUtil.getValueFromJson(TEST_JSON, mKey2);
+        Object jsonVal3 = MPathUtil.getValueFromJson(TEST_JSON, mKey3);
+        /*System.out.println("json 1: "+jsonVal1);
+        System.out.println("json 2: "+jsonVal2);
         System.out.println("JSON Val 3: "+jsonVal3);
+        */
+        Object xmlVal1 = MPathUtil.getValueFromXml(testXml, mKey1);
+        Object xmlVal2 = MPathUtil.getValueFromXml(testXml, mKey2);
+        Object xmlVal3 = MPathUtil.getValueFromXml(testXml, mKey3);
+        /*
+        System.out.println("x1: "+xmlVal1);
+        System.out.println("x2: "+xmlVal2);
+        System.out.println("x Val 3: "+xmlVal3);
+        */
+        Assert.assertArrayEquals(new String[]{jsonVal1.toString(),jsonVal2.toString(),jsonVal3.toString()},
+                        new Object[]{xmlVal1.toString(),xmlVal2.toString(),xmlVal3.toString()});
         
-        String xmlVal3 = MPathUtil.getValueFromXml(testXml, mKey3);
-        System.out.println("XML Val 3: "+xmlVal3);
+    }
+    
+    @Test
+    public void TestCase2() throws IOException, SAXException, XPathExpressionException, ParserConfigurationException{
+        String testJson = (XML.toJSONObject(TEST_XML)).toString();
+        String mKey1 = "Resource.Request[0].method";
+        //System.out.println(TEST_XML);
+        String xmlVal1 = (MPathUtil.getValueFromXml(TEST_XML, mKey1)).toString();
+        String jsonVal1 = (MPathUtil.getValueFromJson(testJson, mKey1)).toString();
         
-        System.out.println(testXml);
+        Assert.assertArrayEquals(new String[]{xmlVal1}, new String[]{jsonVal1});
     }
 }
