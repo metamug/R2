2d39d3ebfff4bd7a5444ecf6ea4eadcbe4e9bd97
Kaisteel <Kaisteel@Lenovo-G505s>
2017-01-28 16:43:58 +0530

    Updated loading file via classLoader

--- src/main/java/com/metamug/parser/RPXParser.java
+++ src/main/java/com/metamug/parser/RPXParser.java
@@ -121,7 +121,7 @@ public class RPXParser {
     }
 
     private void createHtml(Resource resource) throws IOException, FileNotFoundException, XMLStreamException, XPathExpressionException, TransformerException, URISyntaxException {
-        File xsl = new File(getClass().getResource("/resource.xsl").toURI());
+        File xsl = new File(RPXParser.class.getClassLoader().getResource("/resource.xsl").getFile());
         if (!new File(appDirectory + File.separator + appName + File.separator + "docs/v" + resource.getVersion()).exists()) {
             Files.createDirectories(Paths.get(appDirectory + File.separator + appName + File.separator + "docs/v" + resource.getVersion()));
         }
@@ -130,7 +130,7 @@ public class RPXParser {
     }
 
     public Resource parseFromXml() throws JAXBException, SAXException, IOException, FileNotFoundException, XMLStreamException, XPathExpressionException, TransformerException, URISyntaxException {
-        File xsd = new File(getClass().getResource("/resource.xsd").toURI());
+        File xsd = new File(RPXParser.class.getClassLoader().getResource("/resource.xsd").getFile());
         StreamSource xmlFile = new StreamSource(xmlResourceFile);
         SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1");
         Schema schema = schemaFactory.newSchema(xsd);
