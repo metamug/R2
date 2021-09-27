4cd412d6a5d98e357695c519d51260c2314bcf3f
Deepak Mishra <d3ep4k@users.noreply.github.com>
2021-09-27 18:08:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,19 +11,19 @@ rm -rf ./server/webapps/ROOT/assets
 rm -rf ./server/webapps/ROOT/static
 rm -f ./server/webapps/ROOT/index.html
 
+ls ./server/webapps/ROOT/
+
 # Add Empty Folders
 mkdir -p ./server/temp
 mkdir -p ./server/tempapps
 mkdir -p ./server/backend
 mkdir -p ./server/logs
 
-cd console
-mvn clean install
+# Build parser and console
+mvn -f console/pom.xml clean install
+mvn -f parser/pom.xml clean install
+
+# Move console, parser and UI assets into server
 mv console/target/console.war server/webapps
-
-cd ../parser
-mvn clean install
-
-cd ..
 mv parser/target/*.jar server/lib
 mv ui/src/assets server/webapps/ROOT
