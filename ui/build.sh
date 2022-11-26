3fbc6b61d0dcef518e206606b19475a252fedb16
Deepak Mishra <deepak.mishra@metamug.com>
2022-11-26 14:35:21 +0530

    removed ui

--- ui/build.sh
+++
@@ -1,8 +0,0 @@
-cd ui
-npm install
-npm run build
-cd build
-zip -r assets.zip .
-cp assets.zip ../
-rm -f assets.zip
-#ls ../
--- ui/src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-export async function userLoggedIn() {
-  const status = await localStorage.getItem('token')
-  return !!status
-}
-
-export function encodePasswordBase64(password) {
-  return btoa(password)
-}
